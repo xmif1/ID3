39e0fed85f847f8a14faa6c36c96daa7017eb247
xmif1 <xandru.mifsud@gmail.com>
2021-11-22 10:37:40 +0100

    Added prediction scheme

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,3 +1,7 @@
+import Utilities
+import copy
+
+
 class DecisionTreeNode:
     def __init__(self):
         self.split_attr = None
@@ -12,6 +16,67 @@ class DecisionTreeNode:
         self.parent = parent
         self.depth = parent.depth + 1
 
-# class DecisionTree:
-#     def __init__(self, dataset, attributes, target):
-#         self.root = DecisionTreeNode(dataset)
+
+class DecisionTree:
+    def __init__(self, dataset, attributes_dict, target):
+        self.target = target
+        self.attributes_dict = attributes_dict
+        self.root = self._id3(dataset, attributes_dict, target)
+
+    def _id3(self, dataset, attributes_dict, target):
+        root = DecisionTreeNode()
+
+        most_common_val = None
+        max_count = 0
+        for value, count in dataset[target].value_counts().items():
+            if max_count <= count:
+                max_count = count
+                most_common_val = value
+
+            if count == dataset[target].shape[0]:
+                root.classification = value
+                return root
+
+        if len(attributes_dict) == 0:
+            root.classification = most_common_val
+            return root
+
+        root.split_attr = Utilities.best_attribute(dataset, target)
+        for value in attributes_dict[root.split_attr]:
+            subset = dataset.loc[dataset[root.split_attr] == value]
+
+            if subset.shape[0] == 0:
+                child = DecisionTreeNode()
+                child.set_parent(root)
+                child.node_attr = root.split_attr
+                child.node_attr_val = value
+                child.classification = most_common_val
+            else:
+                modified_attr_dict = copy.deepcopy(attributes_dict)
+                modified_attr_dict.pop(root.split_attr)
+
+                child = self._id3(subset, modified_attr_dict, target)
+                child.set_parent(root)
+                child.node_attr = root.split_attr
+                child.node_attr_val = value
+
+            root.children.append(child)
+
+        return root
+
+    def predict(self, predict_attr_dict):
+        for pred_attr, value in predict_attr_dict.items():
+            if pred_attr in self.attributes_dict:
+                if value not in self.attributes_dict[pred_attr]:
+                    raise ValueError("Value " + value + " is not a valid value for attribute " + pred_attr + ".")
+            else:
+                raise ValueError("Attribute " + pred_attr + " is not in the dataset header.")
+
+        curr_node = self.root
+        while curr_node.classification is None:
+            for child in curr_node.children:
+                if child.node_attr_val == predict_attr_dict[child.node_attr]:
+                    curr_node = child
+                    break
+
+        return curr_node.classification
--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-from DecisionTree import DecisionTreeNode
+from DecisionTree import DecisionTree
 import Utilities
 
-import pandas as pd
-import numpy as np
 import argparse
-import copy
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-D", "--data", required=True, help="File path to a .data file")
@@ -15,53 +12,17 @@ ap.add_argument("-f", "--fraction-split", required=True, type=float,
 args = vars(ap.parse_args())
 
 
-def id3(dataset, attributes_dict, target):
-    root = DecisionTreeNode()
-
-    most_common_val = None
-    max_count = 0
-    for value, count in dataset[target].value_counts().items():
-        if max_count <= count:
-            max_count = count
-            most_common_val = value
-
-        if count == dataset[target].shape[0]:
-            root.classification = value
-            return root
-
-    if len(attributes_dict) == 0:
-        root.classification = most_common_val
-        return root
-
-    root.split_attr = Utilities.best_attribute(dataset, target)
-    for value in attributes_dict[root.split_attr]:
-        subset = dataset.loc[dataset[root.split_attr] == value]
-
-        if subset.shape[0] == 0:
-            child = DecisionTreeNode()
-            child.set_parent(root)
-            child.node_attr = root.split_attr
-            child.node_attr_val = value
-            child.classification = most_common_val
-        else:
-            modified_attr_dict = copy.deepcopy(attributes_dict)
-            modified_attr_dict.pop(root.split_attr)
-
-            child = id3(subset, modified_attr_dict, target)
-            child.set_parent(root)
-            child.node_attr = root.split_attr
-            child.node_attr_val = value
-
-        root.children.append(child)
-
-    return root
-
-
 if __name__ == "__main__":
     try:
         train, test, attribute_dict = Utilities.load_dataset(args["data"], args["header"], args["target"],
                                                              train_frac=args["fraction_split"])
-        id3(train, attribute_dict, args["target"])
+
+        decisionTree = DecisionTree(train, attribute_dict, args["target"])
+        # pred_attr_dict = {"outlook": "overcast",
+        #                   "temp": "hot",
+        #                   "humidity": "high",
+        #                   "wind": "weak"}
+        # print(decisionTree.predict(pred_attr_dict))
     except ValueError as ve:
         print(ve)
         print("Exiting...")
